5ee72c696b583149cda32466108ef6bbf5af55e6
Max Ushakov <ushmax@gmail.com>
2024-08-22 13:06:03 +0300

    add cronjob and telegram bot

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,8 @@ RUN apt-get update && apt-get install -y \
     python3 \
     python3-pip \
     ffmpeg \
-    gunicorn && \
+    gunicorn \
+    python3-gdbm && \
     apt-get clean
 
 COPY requirements.txt /app/
@@ -13,12 +14,12 @@ RUN pip3 install -r /app/requirements.txt
 COPY html /app/html/
 COPY static/story.js /app/static/
 COPY server.py /app/
-COPY create_audio.py /app/
+COPY audio.py /app/
+COPY text.py /app/
 COPY create_story.py /app/
 COPY make_topics.py /app/
 COPY stories.py /app/
 COPY topics.py /app/
-# COPY local_init.py /app/
 
 WORKDIR /app
 EXPOSE 8000
--- server.py
+++ server.py
@@ -1,11 +1,24 @@
 from pathlib import Path
 import datetime
 import json
+import shelve
+import os
+import time
 
+import telebot
+from apscheduler.schedulers.background import BackgroundScheduler
+import flask
 from flask import Flask, render_template, redirect, url_for
 
-app = Flask(__name__, static_folder='static', template_folder='html')
+import text
+import audio
 
+app = Flask(__name__, static_folder='static', template_folder='html')
+storage = shelve.open('static/stories/storage')
+print('opened storage', [t for t in storage.items()])
+bot = telebot.TeleBot(os.environ['TELEGRAM_TOKEN'], parse_mode=None)
+
+ROOT = os.environ['STORIA_URL']
 
 @app.route('/')
 def home():
@@ -35,5 +48,55 @@ def show_story_for_date(date):
     return render_template('daily.html', **context)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def generate_story():
+    date = datetime.datetime.now().strftime("%Y-%m-%d")
+    story_fname = f'static/stories/daily-{date}.json'
+    audio_fname = f'static/stories/daily-{date}.mp3'
+
+    story = text.generator.generate_story()
+    story = audio.generator.generate_audio(story, audio_fname)
+    with open(story_fname, 'w') as f:
+        json.dump(story.dict(), f, indent=2)
+
+    story_url = f'{ROOT}/{date}'
+    
+    for user_id, active in storage.items():
+        if active:
+            bot.send_message(int(user_id), "I've created a new story for you! Check it out here: " + story_url)
+
+
+@app.route('/tgwebhook', methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    storage[str(message.from_user.id)] = True
+    storage.sync()
+    bot.reply_to(message, "Cool, hi! I'll send you updates until you turn me off with /stop")
+
+
+@bot.message_handler(commands=['stop'])
+def send_welcome(message):
+    storage[str(message.from_user.id)] = False
+    storage.sync()
+    bot.reply_to(message, "I've turned updates off. See ya!")
+
+
+sched = BackgroundScheduler()
+sched.add_job(generate_story, 'cron', hour=5)
+sched.start()
+
+bot.remove_webhook()
+
+time.sleep(0.1)
+
+# Set webhook
+bot.set_webhook(url=f'{ROOT}/tgwebhook')
